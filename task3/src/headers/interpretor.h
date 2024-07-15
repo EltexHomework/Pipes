819da578d7ed8994380d55171854086b2706db91
TheNorth322 <lol_danchik@bk.ru>
2024-07-16 02:51:25 +0700

    update task3 solution

--- task3/src/headers/interpretor.h
+++ task3/src/headers/interpretor.h
@@ -11,7 +11,12 @@
 #define PATH_LENGTH 128
 #define INPUT_LENGTH 256
 #define FILENAME_LENGTH 128
-#define ARG_LENGTH 64
+#define ARGS_SIZE 16
+
+struct command {
+  char* command;
+  char** args;
+};
 
 struct interpretor {
   char* username;  
@@ -22,10 +27,22 @@ struct interpretor* create_interpretor();
 
 void run_interpretor(struct interpretor* interpretor);
 
-void execute(struct interpretor* interpretor, char* request);
+void execute(struct interpretor* interpretor, struct command** commands, int size);
 
 void change_dir(struct interpretor* interpretor, char* path);
 
 void print_user_info(struct interpretor* interpretor);
+
+void parse(char input[INPUT_LENGTH], struct command** commands, int* size);
+
+void update_username(struct interpretor* interpretor);
+
+void update_dir(struct interpretor* interpretor);
+
+struct command** init_commands();
+
+void dispose(struct interpretor* interpretor);
+
+void clear_commands(struct command** commands);
 
 #endif /* INTERPRETOR_H */
--- task3/src/interpretor.c
+++ task3/src/interpretor.c
@@ -9,67 +9,78 @@ struct interpretor* create_interpretor() {
     exit(EXIT_FAILURE);
   }
   
-  interpretor->username = getlogin();
-  if (interpretor->username == NULL) {
-    perror("getlogin error");
-    exit(EXIT_FAILURE);
-  }
-
-  if (getcwd(interpretor->current_dir, sizeof(interpretor->current_dir)) == NULL) {
-    perror("getcwd error");
-    exit(EXIT_FAILURE);
-  }
+  update_username(interpretor);
+  update_dir(interpretor);
 
   return interpretor;
 }
 
 void run_interpretor(struct interpretor* interpretor) {
   char request[INPUT_LENGTH];
-
+  struct command** commands = init_commands();
+  
+  int size = 0; 
   while (1) {
     print_user_info(interpretor); 
     
     if (fgets(request, sizeof(request), stdin) == NULL) {
       perror("fgets error");
-      return;
+      exit(EXIT_FAILURE);
     }
-    
-    execute(interpretor, request);
+
+    parse(request, commands, &size); 
+    execute(interpretor, commands, size);
   }
 }
 
-void execute(struct interpretor* interpretor, char* request) {
-  char* saveptr;
-  char* request_copy = strdup(request);
-  char* program_name = strtok_r(request, " \n", &saveptr);
+void execute(struct interpretor* interpretor, struct command** commands, int size) {
+  struct command* cur_command = commands[0];
 
-  if (strcmp(program_name, "cd") == 0) {
-    char* path = strtok_r(NULL, " \n", &saveptr);
-    change_dir(interpretor, path);
-    free(request_copy);
+  if (strcmp(cur_command->command, "cd") == 0) {
+    change_dir(interpretor, cur_command->args[1]);
     return;
-  } else if (strcmp(program_name, "exit") == 0) {
-    free(request_copy);
-    free(interpretor);
+  } else if (strcmp(cur_command->command, "exit") == 0) {
+    clear_commands(commands);
+    dispose(interpretor);
     exit(EXIT_SUCCESS);
   }
+  
+  int fd[2];
+  
+  if (pipe(fd) != 0) {
+    perror("pipe error");
+    exit(EXIT_FAILURE);
+  }
 
-  pid_t child = fork();
-  int status;
+  for (int i = 0; i < size; i++) {
+    pid_t child = fork();
 
-  if (child == 0) {
-    if (system(request_copy) != 0) {
-      perror("system error");
+    if (child < 0) {
+      perror("fork error");
+      exit(EXIT_FAILURE);
+    } else if (child == 0) {
+      // stdout is fd0 now
+      dup2(fd[0], 0);
+      close(fd[0]);
+
+      if (i + 1 < size) {
+        // stdin is fd1 now
+        dup2(fd[1], 1);
+        close(fd[1]);
+      }
+
+      if (execvp(commands[i]->command, commands[i]->args) < 0) {
+        perror("exec error");
+        exit(EXIT_FAILURE);
+      }
+      exit(EXIT_SUCCESS);
+    } else {
+      int status;
+      close(fd[1]);
+      if (waitpid(child, &status, 0) == -1) {
+        perror("waitpid error");
+      }
     }
-    free(request_copy);
-    free(interpretor);
-    exit(EXIT_SUCCESS);
-  } else if (child > 0) {
-    while ((child = wait(&status)) > 0);
-    free(request_copy);
-  } else {
-    perror("fork error");
-    free(request_copy);
   }
 }
 
@@ -82,7 +93,73 @@ void change_dir(struct interpretor* interpretor, char* path) {
     perror("chdir error");      
   }
 
+  update_dir(interpretor); 
+}
+
+void parse(char input[INPUT_LENGTH], struct command** commands, int* size) {
+  int command_index = 0;
+  int arg_index = 0;
+  char* token = strtok(input, " \n");
+  
+  if (token == NULL) {
+    perror("strtok error");
+    return;
+  }
+  
+  while (token != NULL) {
+    // found pipeline, parse next command
+    if (strcmp(token, "|") == 0) {
+      commands[command_index]->args[arg_index] = NULL;
+      command_index++;      
+      arg_index = 0;
+    } 
+    // found command arg
+    else {
+      if (arg_index == 0) {
+        commands[command_index]->command = token;
+      } 
+      commands[command_index]->args[arg_index] = token; 
+      arg_index++;       
+    }
+    token = strtok(NULL, " \n");
+  }
+
+  commands[command_index]->args[arg_index] = NULL;
+  *size = command_index + 1;
+}
+
+void update_username(struct interpretor* interpretor) {
+  interpretor->username = getlogin();
+  if (interpretor->username == NULL) {
+    perror("getlogin error");
+    exit(EXIT_FAILURE);
+  }
+}
+
+void update_dir(struct interpretor* interpretor) {
   if (getcwd(interpretor->current_dir, sizeof(interpretor->current_dir)) == NULL) {
     perror("getcwd error");
+    exit(EXIT_FAILURE);
   }
+}
+
+struct command** init_commands() {
+  struct command** commands = (struct command**) malloc(ARGS_SIZE * sizeof(struct command*));
+  
+  for (int i = 0; i < ARGS_SIZE; ++i) {
+    commands[i] = (struct command*) malloc(sizeof(struct command));
+    commands[i]->args = (char**) malloc(ARGS_SIZE * sizeof(char*));
+  }
+  return commands;
+}
+void dispose(struct interpretor* interpretor) {
+  free(interpretor);
+}
+
+void clear_commands(struct command** commands) {
+  for (int i = 0; i < ARGS_SIZE; ++i) {
+    free(commands[i]->args);
+    free(commands[i]);
+  }
+  free(commands);
 }
